a57ab00692d5a3b8595c2a1e430c0363274d06cc
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:42:47 +0300

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,9 @@
+public class Transaction {
+    private String id;
+    private float amount;
+    private TransactionStatus status;
+    private String userId;
+    private PaymentMethod paymentType;
+
+    // Getters and Setters
+}
