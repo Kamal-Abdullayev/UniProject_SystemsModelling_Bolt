a17197b06a4270c7e6e5a2bb1a02469739d2b65d
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:40:13 +0300

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+public abstract class Vehicle {
+    private String id;
+    private CarStatus status;
+    private String vehicleIdentificationNumber;
+    private VehicleType type;
+    private String location;
+    private int range;  // Derived from Electric Vehicle battery percentage
+    private VehicleType vehicleClassType;
+
+    // Getters and Setters
+}
