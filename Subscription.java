eb43b73d11fc7e702902df1fdb37bdba08a310ce
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:42:59 +0300

    Create Subscription.java

---
+++ Subscription.java
@@ -0,0 +1,10 @@
+public class Subscription {
+    private String id;
+    private String name;
+    private String info;
+    private float price;
+    private Date validityPeriod;
+    private VehicleType vehicleType;
+
+    // Getters and Setters
+}
