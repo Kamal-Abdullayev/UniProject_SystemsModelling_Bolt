53cee5b4982730ca71e7f36a7352965f2d7217e6
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:41:34 +0300

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,9 @@
+public class Customer extends User {
+    private String referralCode;
+    private Address homeAddress;
+    private Address workAddress;
+    private PaymentMethod preferredPaymentMethod;
+    private List<Order> rideHistory;
+
+    // Getters and Setters
+}
