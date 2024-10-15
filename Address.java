ec08046dfb221a47df8ee2cdae94b78e790cce2d
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:43:14 +0300

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,10 @@
+public class Address {
+    private String id;
+    private String name;
+    private String city;
+    private String country;
+    private String street;
+    private String apartment;
+
+    // Getters and Setters
+}
