eec1e913e5017d483c5e5c463560b8dab0fe9fe3
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:41:18 +0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,16 @@
+public class User {
+    private String id;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+    private String identityNum;
+    private AccountStatus accountStatus;
+    private String licenceId;
+    private Date birth;
+    private String type;
+    private List<Card> cards;
+    private float rating;
+
+    // Getters and Setters
+}
