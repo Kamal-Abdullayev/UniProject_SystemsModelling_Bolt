16bc897b507e23b23762608e892cbe06b4774699
Feray Gulu-zada <78265800+FerayGuluzada@users.noreply.github.com>
2024-10-15 21:42:00 +0300

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee extends User {
+    private String department;
+    private String role;
+    private int salary;
+    private Date startDate;
+    private int socialSecurityNumber;
+    private int taxNumber;
+
+    // Getters and Setters
+}
